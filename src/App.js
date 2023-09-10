f6e09af7c3e5b7b409f543d039b2fa152eebc454
unknown <himozhima@gmail.com>
2023-09-10 12:03:33 +0300

    Hero-Section

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Navigation from "./Components/Navigation";
+import HeroSection from "./Components/HeroSection" 
+
+function App() {
+  return (
+    <div>
+      
+      <HeroSection />
+    </div>
+  );
+}
+
+export default App;
