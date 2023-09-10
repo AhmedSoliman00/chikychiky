d7b0cec9a0839c91a3e5f0629c9aa0e98c12ca6f
unknown <himozhima@gmail.com>
2023-09-10 13:32:54 +0300

    maxWidth and favIco

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -1,21 +1,24 @@
-import { useState } from 'react'
-import { Dialog } from '@headlessui/react'
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+import { useState } from "react";
+import { Dialog } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 
 const navigation = [
-  { name: 'المتجر', href: '#' },
-  { name: 'جميع المنتجات', href: '#' },
-  { name: 'وصفات', href: '#' },
-  { name: 'الأنتاج', href: '#' },
-]
+  { name: "المتجر", href: "#" },
+  { name: "جميع المنتجات", href: "#" },
+  { name: "وصفات", href: "#" },
+  { name: "الأنتاج", href: "#" },
+];
 
 export default function Example() {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
     <div className="bg-gray-900 h-screen ">
       <header className="absolute inset-x-0 top-0 z-50">
-        <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
+        <nav
+          className="flex items-center justify-between p-6 lg:px-8"
+          aria-label="Global"
+        >
           <div className="flex lg:flex-1">
             <a href="#" className="-m-1.5 p-1.5">
               <span className="sr-only">Your Company</span>
@@ -38,18 +41,30 @@ export default function Example() {
           </div>
           <div className="hidden lg:flex lg:gap-x-12">
             {navigation.map((item) => (
-              <a key={item.name} href={item.href} className="text-base font-semibold leading-6 text-white hover:text-orange-500">
+              <a
+                key={item.name}
+                href={item.href}
+                className="text-base font-semibold leading-6 text-white hover:text-orange-500"
+              >
                 {item.name}
               </a>
             ))}
           </div>
           <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-            <a href="#" className="text-base font-semibold leading-6 text-white hover:text-orange-500">
+            <a
+              href="#"
+              className="text-base font-semibold leading-6 text-white hover:text-orange-500"
+            >
               تسجيل الدخول <span aria-hidden="true">&rarr;</span>
             </a>
           </div>
         </nav>
-        <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+        <Dialog
+          as="div"
+          className="lg:hidden"
+          open={mobileMenuOpen}
+          onClose={setMobileMenuOpen}
+        >
           <div className="fixed inset-0 z-50" />
           <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-orange-500/100 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-white/10">
             <div className="flex items-center justify-between">
@@ -111,25 +126,23 @@ export default function Example() {
             className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-20 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
             style={{
               clipPath:
-                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
             }}
           />
         </div>
-        <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-          <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-            
-          </div>
+        <div className="mx-auto max-w-xl py-32 sm:py-48 lg:py-56 sm:max-w-2xl ">
+          <div className="hidden sm:mb-8 sm:flex sm:justify-center"></div>
           <div className="text-center">
-            <h1 className="text-4xl font-bold tracking-wider text-white sm:text-6xl">
+            <h1 className="text-4xl font-bold tracking-wider text-white sm:text-6xl text-orange-100">
               Chiki Chiki
             </h1>
-            <p className="mt-6 text-lg leading-8 text-gray-300 sm:px-50">
-             Your One-Stop Destination for Farm-Fresh, Mouth-Watering, 
-             and Responsibly Raised Chicken. Experience the Unmatched 
-             Taste of Our Variety of Chicken Dishes, Prepared with Love 
-             and Served with a Smile. Join Us on a Culinary Journey that
-              Celebrates the Richness of Chicken, Straight from the Farm to 
-              Your Plate. Chiki Chiki - Where Flavor Meets Freshness!
+            <p className="mt-6 text-lg leading-8 text-gray-300 lg:text-xl max-sm:px-8">
+              Your One-Stop Destination for Farm-Fresh, Mouth-Watering, and
+              Responsibly Raised Chicken. Experience the Unmatched Taste of Our
+              Variety of Chicken Dishes, Prepared with Love and Served with a
+              Smile. Join Us on a Culinary Journey that Celebrates the Richness
+              of Chicken, Straight from the Farm to Your Plate. Chiki Chiki -
+              Where Flavor Meets Freshness!
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <a
@@ -138,7 +151,6 @@ export default function Example() {
               >
                 أطلب الأن
               </a>
-              
             </div>
           </div>
         </div>
@@ -150,11 +162,11 @@ export default function Example() {
             className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-20 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
             style={{
               clipPath:
-                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
             }}
           />
         </div>
       </div>
     </div>
-  )
+  );
 }
